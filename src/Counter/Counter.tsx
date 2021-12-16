978cc77ed8994f0c1686abe957c404f21852ed42
Kravchenko Andrii <yazaandrey@gmail.com>
2021-12-16 23:32:53 +0200

    Counter v1

--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -3,51 +3,91 @@ import s from './Counter.module.css'
 
 
 function Counter() {
-    let [maxValue, setMaxValue] = useState(0)
-    let [count, setCount] = useState(0)
-    let [error, setError] = useState(false)
+    const [maxValue, setMaxValue] = useState<number>(5)
+    const [minValue, setMinValue] = useState<number>(0)
+    const [count, setCount] = useState<number>(0)
+    const [error, setError] = useState<boolean>(false)
+    const [errorMessage, setErrorMessage] = useState<string>('')
+    const [message, setMessage] = useState<string>('')
+    const [isActiveSetting, setIsActiveSetting] = useState<boolean>(false)
+    const [isActiveResetButton, setIsActiveResetButton] = useState<boolean>(false)
 
-    const setNumber = () => {
 
-    }
+    const maxNumber = 5;
+
     const maxValueChange = (e: ChangeEvent<HTMLInputElement>) => {
-        let text = e.currentTarget.value
-
-    }
-    const minValueChange = (e: ChangeEvent<HTMLInputElement>) => {
-
-    }
-    const incCallback = () => {
-
-        if (count < 5) {
-            setCount(count + 1)
+        setMaxValue(+e.currentTarget.value)
+        setIsActiveSetting(true)
+        setCount(0)
+        if(minValue >= +e.currentTarget.value || 0 > +e.currentTarget.value){
+            setErrorMessage('Неверное значение')    
         } else {
-            setError(true)
+            setErrorMessage('')
+            setMessage('Подтвердите выбранное значение')
         }
     }
-    const resetCallback = () => {
+
+    const minValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setMinValue(+e.currentTarget.value)
+        setIsActiveSetting(true)
         setCount(0)
+        if(maxValue <= +e.currentTarget.value || 0 > +e.currentTarget.value){
+            setErrorMessage('Неверное значение')    
+        } else {
+            setErrorMessage('')
+            setMessage('Подтвердите выбранное значение')
+        }
     }
+    
+    const setNumber = () => {
+        setCount(minValue)
+        setMessage('')
+        setIsActiveSetting(false)
+        setIsActiveResetButton(true)
+    }
+
+    const incCallback = () => {
+        setCount((prev:number) => prev + 1)
+        setIsActiveResetButton(false)
+    }
+
+    const resetCallback = () => {
+        setCount(minValue)
+        setIsActiveResetButton(true)
+    }
+
     return (
         <div className={s.maincontainer}>
             <div className={s.item}>
                 <div className={s.block1}>
                     <div className={s.inputStyle1}>
-                        max value: <input type={"number"}
-                                          value={""}
-                                          onChange={maxValueChange}/>
-                        min value:<input type='number'
-                                         onChange={minValueChange}/>
+                        max value: <input 
+                                        type={"number"}
+                                        value={maxValue}
+                                        onChange={maxValueChange}
+                                    />
+                        min value: <input 
+                                        type='number'
+                                        value={minValue} 
+                                        onChange={minValueChange}
+                                    />
                     </div>
                     <div className={s.setButton}>
-                        <button onClick={setNumber}>Set</button>
+                        <button disabled={!!errorMessage || !isActiveSetting} onClick={setNumber}>Set</button>
                     </div>
                 </div>
                 <div className={s.block2}>
-                    <div className={`${s.sValue} ${error ? s.redNumber : ''}`}>{count}</div>
+                    <div 
+                        className={`
+                            ${s.sValue} 
+                            ${errorMessage ? s.redNumber : ''}
+                            ${count >= maxValue ? s.redNumber : ''}
+                        `}>
+                        {errorMessage ? errorMessage : message ? message : count}
+                    </div>
                     <div className={s.inc_reset}>
-                        <button disabled={error} onClick={incCallback}>Inc</button>
-                        <button onClick={resetCallback}>Reset</button>
+                        <button disabled={isActiveSetting || count === maxValue} onClick={incCallback}>Inc</button>
+                        <button disabled={isActiveSetting || isActiveResetButton} onClick={resetCallback}>Reset</button>
                     </div>
                 </div>
             </div>
